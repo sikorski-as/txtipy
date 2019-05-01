51ce21718dda16801215e53e05f3a9cdb4a6632d
sikorski-as <sikorski-as@users.noreply.github.com>
2019-05-01 20:09:00 +0200

    Minor example.py changes
    Names of functions changed after changes in Page class.

--- examples.py
+++ examples.py
@@ -11,7 +11,7 @@ except PageAlreadyExists as e:
 input('press enter to continue')
 	
 try:
-	status = page.change_content('#txtipy example with changed content')
+	status = page.set_content('#txtipy example with changed content')
 	print('updated content:', status)
 except PageDoesNotExist as e:
 	print(e)
@@ -19,7 +19,7 @@ except PageDoesNotExist as e:
 input('press enter to continue')	
 	
 try:
-	status = page.change_title('txtipy example - with title')
+	status = page.set_title('txtipy example - with title')
 	print('title updated:', status)
 except PageDoesNotExist as e:
 	print(e)
